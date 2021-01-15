14424a16549158421311fbe3cb5065ab8427282e
nematu <nayaz-akhundzada@mail.greenriver.edu>
2021-01-15 15:23:22 -0500

    index.php added

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+/* Nematullah
+* 01/15/2021
+ *
+ */
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+$numbers
+function printArr2($numbers)
+{
+    foreach ($numbers as $item =>$price)
+    {
+        echo $price.'<br>';
+    }
+}
+printArr2($numbers);
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pair Program 2</title>
+</head>
+<body>
+<?php
+echo "Step: 1 - PHP Array Practice";
+
+?>
+
+</body>
+</html>
